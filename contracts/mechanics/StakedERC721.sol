d5efa04c6b183e95f5926055432fa338ef74f89a
durienb <damien@wilderworld.com>
2023-10-05 10:39:16 -0600

    folder structure

--- contracts/mechanics/StakedERC721.sol
+++ contracts/mechanics/StakedERC721.sol
@@ -4,8 +4,8 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Wrapper.sol";
 import "../interfaces/IStakedERC721.sol";
 
 contract StakedERC721 is ERC721Wrapper, IStakedERC721 {
-    constructor(IERC721 underlyingToken) 
-    ERC721("StakedNFT", "SNFT")
+    constructor(IERC721 underlyingToken, string memory name, string memory symbol) 
+    ERC721(name, symbol)
     ERC721Wrapper(underlyingToken){
     }
 }
