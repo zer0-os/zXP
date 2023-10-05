8ba8c28376f8947dcf08795924111caa7ec47e21
durienb <damien@wilderworld.com>
2023-10-05 10:58:39 -0600

    format eslint, no empty off

--- contracts/mechanics/StakedERC721.sol
+++ contracts/mechanics/StakedERC721.sol
@@ -1,11 +1,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Wrapper.sol";
-import "../interfaces/IStakedERC721.sol";
+import "./interfaces/IStakedERC721.sol";
 
 contract StakedERC721 is ERC721Wrapper, IStakedERC721 {
-    constructor(IERC721 underlyingToken, string memory name, string memory symbol) 
-    ERC721(name, symbol)
-    ERC721Wrapper(underlyingToken){
-    }
+    constructor(
+        IERC721 underlyingToken,
+        string memory name,
+        string memory symbol
+    ) ERC721(name, symbol) ERC721Wrapper(underlyingToken) {}
 }
