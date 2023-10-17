9d3237f2c2ba8b611cc64bb5ca977aaecb3dc4c6
durienb <damien@wilderworld.com>
2023-10-17 00:36:43 -0600

    reduce game vault

--- contracts/zxp/game/GameVault.sol
+++ contracts/zxp/game/GameVault.sol
@@ -8,25 +8,10 @@ import {IGameVault} from "./interfaces/IGameVault.sol";
 import {GameRegistryClient} from "../GameRegistryClient.sol";
 
 contract GameVault is ERC721Wrapper, Ownable, IGameVault, GameRegistryClient {
-    bytes32 internal constant SEASON_REGISTRY = "SeasonRegistry";
-    IERC20 private rewardToken;
-    uint public numStaked;
-    mapping(uint season => uint rewards) public seasonRewards;
-
     constructor(
         IERC721 underlyingToken,
         IERC20 _rewardToken,
         string memory name,
         string memory symbol
-    ) ERC721(name, symbol) ERC721Wrapper(underlyingToken) {
-        rewardToken = _rewardToken;
-        Ownable(msg.sender);
-    }
-
-    function claimRewards(
-        address to,
-        uint season
-    ) external override only(SEASON_REGISTRY) {
-        rewardToken.transfer(to, seasonRewards[season]);
-    }
+    ) ERC721(name, symbol) ERC721Wrapper(underlyingToken) {}
 }
