4f91dafd8864448778ab0a4bc5d110474ec1f7d6
durienb <damien@wilderworld.com>
2023-11-21 13:48:45 -0700

    test repair

---
+++ contracts/zxp/game/LogarithmicLevelCurve.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {IGameRegistry} from "../interfaces/IGameRegistry.sol";
+import {GameRegistryClient} from "../GameRegistryClient.sol";
+import {ILevelCurve} from "./interfaces/ILevelCurve.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract QuadraticLevelCurve is ILevelCurve {
+    using Math for uint256;
+
+    uint private intercept;
+    uint private coefficient;
+
+    constructor(
+        uint initialCoefficient,
+        uint initialIntercept
+    ) {
+        coefficient = initialCoefficient;
+        intercept = initialIntercept;
+    }
+
+    function xpRequired(uint256 level) public view override returns (uint256) {
+        return coefficient * Math.log2(level)
+    }
+
+    function levelAt(uint256 xp) external view override returns (uint) {
+        return coefficient * xp * xp;
+    }
+}
--- test/zXP.test.ts
+++ test/zXP.test.ts
@@ -1,19 +1,16 @@
 import { ethers } from "hardhat";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-//import { ethers, Signer } from "ethers";
 import { expect } from "chai";
 import { GameRegistry, GameVault, MockERC20, MockERC721, PlayerRewards, SeasonRegistry, StakerRewards, XP } from "../typechain";
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require("@nomicfoundation/hardhat-chai-matchers");
 
 
 describe("ZXP", () => {
-    let deployer: Signer;
-    let official: Signer;
-    let player1: Signer;
-    let player2: Signer;
-    let player3: Signer;
+    let deployer: any;
+    let official: any;
+    let player1: any;
+    let player2: any;
+    let player3: any;
     let p1: string;
     let p2: string;
     let p3: string;
@@ -21,8 +18,8 @@ describe("ZXP", () => {
     let s2: string;
     let officialAddress: string;
     let deployerAddress: string;
-    let staker1: Signer;
-    let staker2: Signer;
+    let staker1: any;
+    let staker2: any;
     let mockErc20: MockERC20;
     let mockErc721: MockERC721;
     let top3Rewards: PlayerRewards;
@@ -47,46 +44,45 @@ describe("ZXP", () => {
     let s2nft = 2;
 
     before(async () => {
-        //const provider = new JsonRpcProvider("127.0.0.1:8545");
         [deployer, official, player1, player2, player3, staker1, staker2] = await ethers.getSigners();
 
-        const erc20Contracts = await hre.ethers.getContractFactory("MockERC20");
+        const erc20Contracts = await ethers.getContractFactory("MockERC20");
         const erc20 = await erc20Contracts.deploy("zToken", "WILD");
         await erc20.waitForDeployment();
         mockErc20 = erc20;
 
-        const erc721Contracts = await hre.ethers.getContractFactory("MockERC721");
+        const erc721Contracts = await ethers.getContractFactory("MockERC721");
         const erc721 = await erc721Contracts.deploy("zToken", "WILD", "");
         await erc721.waitForDeployment();
         mockErc721 = erc721;
 
-        const gameRegFactory = await hre.ethers.getContractFactory("GameRegistry");
+        const gameRegFactory = await ethers.getContractFactory("GameRegistry");
         const gameRegDeploy = await gameRegFactory.deploy();
         await gameRegDeploy.waitForDeployment();
         gameRegistry = gameRegDeploy;
 
-        gameName = ethers.encodeBytes32String("game");
-        const seasonRegFactory = await hre.ethers.getContractFactory("SeasonRegistry");
+        gameName = ethers.utils.formatBytes32String("game");
+        const seasonRegFactory = await ethers.getContractFactory("SeasonRegistry");
         const seasonRegDeploy = await seasonRegFactory.deploy(gameRegistryAddress, gameName);
         await seasonRegDeploy.waitForDeployment();
         seasonRegistry = seasonRegDeploy;
 
-        const gameVaultFactory = await hre.ethers.getContractFactory("GameVault");
+        const gameVaultFactory = await ethers.getContractFactory("GameVault");
         const gameVaultDeploy = await gameVaultFactory.deploy(mockErc721Address, mockErc20Address, "StakedNFT", "SNFT", gameRegistryAddress, gameName);
         await gameVaultDeploy.waitForDeployment();
         gameVault = gameVaultDeploy;
 
-        const xpFactory = await hre.ethers.getContractFactory("XP");
+        const xpFactory = await ethers.getContractFactory("XP");
         const xpDeploy = await xpFactory.deploy("zXP", "XP", gameRegistryAddress, gameName);
         await xpDeploy.waitForDeployment();
         xp = xpDeploy;
 
-        const top3rewardsFactory = await hre.ethers.getContractFactory("PlayerRewards");
+        const top3rewardsFactory = await ethers.getContractFactory("PlayerRewards");
         const top3deploy = await top3rewardsFactory.deploy(officialAddress, mockErc20Address, seasonRegistryAddress, "0", "100");
         await top3deploy.waitForDeployment();
         top3Rewards = top3deploy;
 
-        const stakerRewardsFactory = await hre.ethers.getContractFactory("StakerRewards");
+        const stakerRewardsFactory = await ethers.getContractFactory("StakerRewards");
         const stakerRewardsDeploy = await stakerRewardsFactory.deploy(mockErc20Address, "10", gameVaultAddress, gameVaultAddress, seasonRegistryAddress, "0");
         await stakerRewardsDeploy.waitForDeployment();
         stakerRewards = stakerRewardsDeploy;
@@ -104,27 +100,27 @@ describe("ZXP", () => {
         await gameRegistry.createGame(gameName, deployerAddress, "description", [], []);
     });
     it("Creates game", async () => {
-        const sr = ethers.encodeBytes32String("SeasonRegistry");
+        const sr = ethers.utils.formatBytes32String("SeasonRegistry");
         const names = [sr];
         const objects = [seasonRegistryAddress];
 
-        const tg = ethers.encodeBytes32String("TestGame");
+        const tg = ethers.utils.formatBytes32String("TestGame");
         await gameRegistry.createGame(tg, deployerAddress, "description", names, objects);
     });
     it("Registers GameVault", async () => {
-        const gameVaultBytes = ethers.encodeBytes32String("GameVault");
+        const gameVaultBytes = ethers.utils.formatBytes32String("GameVault");
         await gameRegistry.registerObjects(gameName, [gameVaultBytes], [gameVaultAddress]);
     });
     it("Registers XP", async () => {
-        const xpBytes = ethers.encodeBytes32String("XP");
+        const xpBytes = ethers.utils.formatBytes32String("XP");
         await gameRegistry.registerObjects(gameName, [xpBytes], [xpAddress]);
     });
     it("Registers SeasonRegistry", async () => {
-        const sr = ethers.encodeBytes32String("SeasonRegistry");
+        const sr = ethers.utils.formatBytes32String("SeasonRegistry");
         await gameRegistry.registerObjects(gameName, [sr], [seasonRegistryAddress]);
     });
     it("Registers PlayerRewards", async () => {
-        const pr = ethers.encodeBytes32String("PlayerRewards");
+        const pr = ethers.utils.formatBytes32String("PlayerRewards");
         await seasonRegistry.registerMechanics([pr], [top3RewardsAddress]);
     });
     const numSeasons = 3
@@ -138,7 +134,7 @@ describe("ZXP", () => {
             await mockErc721.connect(staker1)["safeTransferFrom(address,address,uint256)"](s1, gameVaultAddress, s1nft);
         });
         it("Mints Staker 2 NFT", async () => {
-            //s2nft = ethers.keccak256(ethers.encodeBytes32String((index + 1).toString()));
+            //s2nft = ethers.keccak256(ethers.utils.formatBytes32String((index + 1).toString()));
             s2nft = s2nft + 3;
             console.log(s2nft);
             await mockErc721.connect(deployer).mint(s2, s2nft);
@@ -155,7 +151,7 @@ describe("ZXP", () => {
             //await mockErc20.connect(deployerAddress)["transfer(address,uint256)"](stakerRewardsAddress, "1000000000000000000000000");
         });
         it("Registers StakerReward mechanic", async () => {
-            const sr = ethers.encodeBytes32String("StakerRewards");
+            const sr = ethers.utils.formatBytes32String("StakerRewards");
             await seasonRegistry.registerMechanics([sr], [stakerRewardsAddress]);
         });
         it("Starts the season", async () => {
@@ -181,18 +177,18 @@ describe("ZXP", () => {
             });
         }
         it("Player 1 claims season rewards", async () => {
-            //await top3Rewards.connect(player1).claim(p1);
+            await top3Rewards.connect(player1).claim(p1);
             expect((await mockErc20.balanceOf(p1)).toString() == firstReward);
         });
         it("Staker 1 unstakes and claims rewards", async () => {
-            //await gameVault.connect(staker1).withdrawTo(s1, [s1nft]);
+            await gameVault.connect(staker1).withdrawTo(s1, [s1nft]);
         });
         it("Staker 2 claims rewards without unstaking", async () => {
-            //await stakerRewards.connect(staker2).claim(s2nft);
+            await stakerRewards.connect(staker2).claim(s2nft);
         });
         it("Awards xp to stakers", async () => {
-            //console.log(await xp.balanceOf(staker1Address));
-            //console.log(await xp.balanceOf(staker2Address));
+            console.log(await xp.balanceOf(s1));
+            console.log(await xp.balanceOf(s2));
         });
         it("Ends the season", async () => {
             await seasonRegistry.endSeason();
